a5b040859f500d58280721d7c4da6b7f387ebf4b
SKARHM <42718026+SKARHM@users.noreply.github.com>
2018-09-04 14:09:27 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+شرفنا يا عسل:wink: :kissing_heart:
+**
+حط الرابط هنا
+                         
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+شرفنا يا عسل:wink: :kissing_heart:
+**
+حط الرابط هنا
+                         
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login('token');

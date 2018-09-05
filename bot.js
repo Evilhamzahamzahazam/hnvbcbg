485f45f8c4cd366aeb31d4ba92e55e4cef0a8f9a
SKARHM <42718026+SKARHM@users.noreply.github.com>
2018-09-05 11:59:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,60 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const bot = new Discord.Client();
-client.on('ready', () => {
-  console.log('╔[════════════════════════════════════]╗');
-  console.log('            ╔[════════════]╗')
-  console.log('              Bot Is Online')
-  console.log('            ╚[════════════]╝')
-  console.log('╚[════════════════════════════════════]╝')
-});
 
-console.log("Welcome Again !");
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+
+
+console.log("Welcome Again  by Fox!");
+
+
+Client.on("guildMemberRemove", member => {
+  member.send(`
+**اهلن فيك في سيرفرنا  تشرفنا 
+:heart:  سوف تقام فعاليت كبيره  والسيرفر قيد التحديث 
+:kissing_heart:  واتمنه تشرفونا و تدخلون 
+
+ادخل للسيرفر  
+ https://discord.gg/jXG6Md
+**
+***========================================***
+** Ahnk you in our server, we are honored
+:heart: will be a significant effectiveness and the server is under update
+:kissing_heart: And let us have a good time
+
+join server  
+ https://discord.gg/jXG6Md
+
+
+
+**
+[${member}]`);
+
+  });
  
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
+
+  Client.on('guildMemberAdd', member => {
+  member.send(`
+**اهلن فيك في سيرفرنا  تشرفنا 
+:heart:  سوف تقام فعاليت كبيره  والسيرفر قيد التحديث 
+:kissing_heart:  واتمنه تشرفونا و تدخلون 
+
+ادخل للسيرفر  
+ https://discord.gg/jXG6Md
+**
+***========================================***
+** Ahnk you in our server, we are honored
+:heart: will be a significant effectiveness and the server is under update
+:kissing_heart: And let us have a good time
+
+join server  
+ https://discord.gg/jXG6Md
 
 
-//https://discord.gg/jXG6Md
+**
+[${member}]`);
 
 
- ${member}  
-`)
-}).catch(console.error)
-
-})
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-
-
-
-//https://discord.gg/jXG6Md
-
-
- ${member}  
-`) 
-}).catch(console.error)
-})
+  });
 
 
 
-
-
-client.login('  توكن بوتك هنا   ');
+Client.login("NDg2ODE4MzgzMzU5MjQ2MzQ0.DnEosQ.93NAMvtdHqdAJwZ20mSnQ_aWN9Y");
